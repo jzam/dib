97e4681b9dbb4936d1cbec0cd00267d30ccd6f07
jzam <jzaman03@gmail.com>
2015-10-13 12:07:23 -0700

    Added directory recreation and put all files under a new root directory

--- dib.c
+++ dib.c
@@ -1,9 +1,12 @@
 #include <tsk/libtsk.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define NUM_ARGS 4
 #define ERROR -1
 #define SUCCESS 0
+#define MAX_PATH 260
 
 //Construct list of used partitions, so far this program has only been tested with single-partition disks...
 void buildPartList(TSK_VS_PART_INFO *allPartList, TSK_VS_PART_INFO **partListHead) {
@@ -68,66 +71,138 @@ void printDirectory(TSK_VS_PART_INFO *vsInfo, char *dirPath, TSK_INUM_T inum) {
    tsk_fs_close(fs);
 }
 
-//Copies file from disk image to an output file - Hardcoded for testing
-void copyFile(TSK_VS_PART_INFO *vsInfo, char *dirPath, TSK_INUM_T inu, char *fileName) {
+void copyFile(TSK_FS_INFO *fs, char *filePath, char *fileName) {
+   TSK_FS_FILE *file;
+   TSK_FS_DIR *dir;
+   FILE *fp;
+   char *buf;
+   char *fullPath;
+   char *newPath;
+   int i;
+   int err;
+   ssize_t bytesRead;
+   size_t check;
+   //size_t len;
+
+   if (filePath == NULL)
+      printf("EMPTY file path\n");
+   if (fileName == NULL)
+      printf("EMPTY file Name\n");
+
+   fullPath = malloc(MAX_PATH);
+   strcpy(fullPath, filePath);
+
+   //File path with '/' appended for creating correct path names
+   if (strcmp(fullPath, "/"))
+      strcat(fullPath, "/");
+
+   //len = strlen(filePath);
+   //printf("hello\n");
+   //printf("file path: %s\n", strcat(filePath, fileName));
+   //Open the file
+   printf("hi\n");
+   file = tsk_fs_file_open(fs, NULL, strcat(fullPath, fileName));
+   printf("hi\n");
+   if (file == NULL) 
+      printf("Problem opening file: %s\n", fullPath);
+
+   printf("Full path is %s\n", fullPath);
+   newPath = &(fullPath[1]);
+   printf("New path is %s\n", newPath);
+   if (file->meta->type == TSK_FS_META_TYPE_DIR) {
+      printf("%s was a directory\n", fileName);
+      printf("Hey-o\n");
+
+      dir = tsk_fs_dir_open(fs, fullPath);
+      if (!dir)
+         printf("ERROR opening directory\n");
+
+      printf("Attempting to make directory %s\n", fullPath);
+      err = mkdir(newPath, S_IRWXU | S_IRWXG | S_IRWXO);
+   
+      if (err < 0)
+         printf("ERROR: Could not make directory\n");
+
+      //Recursive call to copy files in this directory
+      for (i = 0; i < dir->names_used; i++) {
+         if (strcmp(".", dir->names[i].name) && strcmp("..", dir->names[i].name)) {
+            printf("%s\n", dir->names[i].name);
+            copyFile(fs, fullPath, dir->names[i].name);
+            //printf("%s - inum: %d\n", dir->names[i].name, (int)dir->names[i].meta_addr);
+         }
+      }
+      //exit(0);
+   }
+   else {
+      printf("Attempting to create file %s\n", fullPath);
+      //Allocate memory to hold file contents
+      buf = malloc(file->meta->size);
+
+      //Read contents of disk file into buffer
+      bytesRead = tsk_fs_file_read(file, 0, buf, file->meta->size, TSK_FS_FILE_READ_FLAG_SLACK);
+   
+      //Create a new file
+      fp = fopen(newPath, "w");
+
+      //Write contents into file
+      check = fwrite(buf, bytesRead, 1, fp);
+      if (check < 0)
+         printf("ERROR writing file\n");
+
+      //Close the new file
+      fclose(fp);
+
+      //Free memory  
+      free(buf);
+
+      //Close file object
+      tsk_fs_file_close(file);
+   }
+}
+void copyAllFiles(TSK_VS_PART_INFO *vs) {
    TSK_FS_INFO *fs;
    TSK_FS_DIR *dir;
-   TSK_FS_FILE *file;
+   TSK_FS_FILE *root;
    TSK_INUM_T inum;
    FILE *fp;
    int i;   
+   int err;
    char *buf;
+   char *filePath = malloc(MAX_PATH);
    ssize_t bytesRead;
    size_t check;
 
    //Open file system object
-   fs = tsk_fs_open_vol(vsInfo, TSK_FS_TYPE_DETECT);
+   fs = tsk_fs_open_vol(vs, TSK_FS_TYPE_DETECT);
+
+   //Set file path to root
+   strcpy(filePath, "/");
 
    if (fs == NULL) {
       printf("Could not open fs. :(\n");
    }
 
-   //Open directory object with directory path or inum
-   if (dirPath != NULL) {
-      dir = tsk_fs_dir_open(fs, dirPath);
-      if (dir == NULL) {
-         printf("ERROR: Attempt to open non-directory @ %s\n", dirPath);
-         exit(0);
+   dir = tsk_fs_dir_open(fs, "");
+   if (!dir)
+      printf("ERROR opening root directory\n");
+
+   err = mkdir("root", S_IRWXU | S_IRWXG | S_IRWXO);
+   
+   if (err < 0)
+      printf("ERROR: Could not make directory\n");
+
+   err = chdir("root");
+   if (err < 0)
+      printf("ERROR: Could not change directory\n");
+
+   //Prints all files in directory including inum
+   for (i = 0; i < dir->names_used; i++) {
+      if (strcmp(".", dir->names[i].name) && strcmp("..", dir->names[i].name)) {
+         printf("%s\n", dir->names[i].name);
+         copyFile(fs, filePath, dir->names[i].name);
       }
    }
-   else {
-      dir = tsk_fs_dir_open_meta(fs, inu);
-      if (dir == NULL) {
-         printf("ERROR: Attempt to open non-directory @ inum %d\n", (int) inu);
-         exit(0);
-      }
-   }
-
-   inum = dir->names[2].meta_addr;
-   printf("inum is %d\n", (int)inum);
-   file = tsk_fs_file_open_meta(fs, NULL, inum);
-   if (file == NULL) 
-      printf("!File is NULL!\n");
-   
-   //Allocate memory to hold file contents
-   buf = malloc(file->meta->size);
-
-   //printf("Values passed to file read function: Off: %d, Size: %d\n", (int)file->fs_info->offset, (int)file->meta->size); 
-   bytesRead = tsk_fs_file_read(file, 0, buf, file->meta->size, TSK_FS_FILE_READ_FLAG_SLACK);
-   
-   /*printf("Offset: %d, Size: %d, Flags: %0x\n", (int)file->fs_info->offset, (int)file->meta->size, (int)file->meta->flags);
-
-   if (bytesRead < 0)
-      printf("ERROR reading file\n");
-   else
-      printf("Read %d bytes\n", (int)bytesRead);
-   */
-   fp = fopen(fileName, "w");
-   check = fwrite(buf, bytesRead, 1, fp);
-   if (check <= 0)
-      printf("ERROR writing file\n");
-   fclose(fp);  
-   free(buf);
+   tsk_fs_dir_close(dir);
 }
 
 int main(int argc, char **argv) {
@@ -190,11 +265,15 @@ int main(int argc, char **argv) {
    //Build list of partitions with valid file systems
    buildPartList(allPartList, &partList);
 
+   printf("Attempting to copy all files\n");
+   //Copy ALL the files
+   copyAllFiles(partList);
+
    //Print input directory
    printDirectory(partList, dirPath, inu);
 
    //Copy file to output - Hardcoded for testing right now
-   copyFile(partList, dirPath, inu, "out.txt");
+   //copyFile(partList, dirPath, inu, "out.txt");
 
    //Close disk objects
    tsk_vs_close(vs);
